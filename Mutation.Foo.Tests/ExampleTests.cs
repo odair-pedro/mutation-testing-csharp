7c2b2311dc4ec10ea128779079964b73852532a7
Odair Pedro <brhbs0455@ambev.com.br>
2020-06-30 20:20:54 -0300

    Change to survive mutation

--- Mutation.Foo.Tests/ExampleTests.cs
+++ Mutation.Foo.Tests/ExampleTests.cs
@@ -5,10 +5,17 @@ namespace Mutation.Foo.Tests
     public class ExampleTests
     {
         [Fact]
-        public void Concat_ShouldReturn_CorrectValue()
+        public void ConcatValues_ShouldReturn_CorrectValue()
         {
             var result = Example.ConcatValues("foo", "bar");
             Assert.Equal("foobar", result);
+        }
+
+        [Fact]
+        public void RemoveChars_ShouldReturn_CorrectValue()
+        {
+            var result = Example.RemoveChars("123456789");
+            Assert.Equal(string.Empty, result);
         }
     }
 }
--- Mutation.Foo/Example.cs
+++ Mutation.Foo/Example.cs
@@ -1,10 +1,17 @@
-﻿namespace Mutation.Foo
+﻿using System.Text.RegularExpressions;
+
+namespace Mutation.Foo
 {
     public static class Example
     {
         public static string ConcatValues(params string[] values)
         {
             return string.Join(string.Empty, values);
+        }
+
+        public static string RemoveChars(string value)
+        {
+            return Regex.Match(value, @"\D+").Value;
         }
     }
 }
