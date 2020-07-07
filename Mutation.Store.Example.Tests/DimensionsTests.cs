da133393a5cdb51725c407f056a4c8fa593ad5dd
Odair Pedro <brhbs0455@ambev.com.br>
2020-07-07 13:54:31 -0300

    Killing mutants

--- Mutation.Store.Example.Tests/DimensionsTests.cs
+++ Mutation.Store.Example.Tests/DimensionsTests.cs
@@ -5,13 +5,27 @@ namespace Mutation.Store.Example.Tests
 {
     public class DimensionsTests
     {
-        [Fact]
-        public void ToString_WithCorrectDimensions_ShouldReturn_CorrectValue()
+        [Theory]
+        [InlineData(10, 10, 10)]
+        public void ToString_WithCorrectDimensions_ShouldReturn_CorrectValue(decimal height, decimal width, decimal depth)
         {
-            var dimensions = new Dimensions(10, 10, 10);
+            var dimensions = new Dimensions(height, width, depth);
             var result = dimensions.ToString();
             
-            Assert.Equal("HxWxD: 10x10x10", result);
+            Assert.Equal($"HxWxD: {height}x{width}x{depth}", result);
+        }
+        
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(0, 10, 10)]
+        [InlineData(10, 0, 10)]
+        [InlineData(10, 10, 0)]
+        public void ToString_WithIncorrectDimensions_ShouldReturn_Message(decimal height, decimal width, decimal depth)
+        {
+            var dimensions = new Dimensions(height, width, depth);
+            var result = dimensions.ToString();
+            
+            Assert.Equal("No dimensions", result);
         }
 
         [Fact]
@@ -19,7 +33,23 @@ namespace Mutation.Store.Example.Tests
         {
             var dimentions = new Dimensions(0, 10, 10);
 
-            Assert.Throws<Exception>(() => dimentions.Validate());
+            Assert.Equal("Height can not be equal or less than zero", Assert.Throws<Exception>(() => dimentions.Validate()).Message);
+        }
+        
+        [Fact]
+        public void Validate_WithIncorrectWidth_ShouldThrowException()
+        {
+            var dimentions = new Dimensions(10, 0, 10);
+
+            Assert.Equal("Width can not be equal or less than zero", Assert.Throws<Exception>(() => dimentions.Validate()).Message);
+        }
+        
+        [Fact]
+        public void Validate_WithIncorrectDepth_ShouldThrowException()
+        {
+            var dimentions = new Dimensions(10, 10, 0);
+
+            Assert.Equal("Depth can not be equal or less than zero", Assert.Throws<Exception>(() => dimentions.Validate()).Message);
         }
     }
 }
